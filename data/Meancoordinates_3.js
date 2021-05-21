7108220c1371bb9ac3b66d31f63001c35b96d1b4
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-05-21 21:36:34 +0700

    Add files via upload

---
+++ data/Meancoordinates_3.js
@@ -0,0 +1,8 @@
+var json_Meancoordinates_3 = {
+"type": "FeatureCollection",
+"name": "Meancoordinates_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "MEAN_X": 106.80167570625001, "MEAN_Y": -6.6011111937500013 }, "geometry": { "type": "Point", "coordinates": [ 106.801675706250009, -6.601111193750001 ] } }
+]
+}
---
+++ data/PointofInterestBogor_2.js
@@ -0,0 +1,23 @@
+var json_PointofInterestBogor_2 = {
+"type": "FeatureCollection",
+"name": "PointofInterestBogor_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Toge Goreng Ibu Hj. Omah", "Desc": "RT.02\/RW.04, Sempur, Bogor City, West Java", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.79674, -6.58864 ] } },
+{ "type": "Feature", "properties": { "Name": "Kebun Raya Bogor", "Desc": "Jalan Otto Iskandardinata, RT.04\/RW.02, Paledang, Bogor City, West Java", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.7988074, -6.6030394 ] } },
+{ "type": "Feature", "properties": { "Name": "Macaroni Panggang", "Desc": "Jalan Salak, RT.03\/RW.04, Babakan, Bogor City, West Java", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.802645346654373, -6.588911405865043 ] } },
+{ "type": "Feature", "properties": { "Name": "The Bucket List", "Desc": "Jalan R. Haji Muhammad Tohir, RT.07\/RW.10, Tanah Baru, Bogor City, West Java", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.815810753124509, -6.591191635276733 ] } },
+{ "type": "Feature", "properties": { "Name": "Es Podeng dan Es Doger Regina", "Desc": "Jalan Rangga Gading, RT.01\/RW.02, Gudang, Bogor City, West Java", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.800424263125038, -6.605732572629196 ] } },
+{ "type": "Feature", "properties": { "Name": "Soto Mie Pak Kumis", "Desc": "Suryakencana Street, RT.02\/RW.02, Babakan Pasar, Bogor City, West Java", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.802993606948377, -6.60846184380557 ] } },
+{ "type": "Feature", "properties": { "Name": "Martabak A-Choy (Martabak Encek)", "Desc": "Suryakencana Street, RT.01\/RW.07, Babakan, Bogor City, West Java", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.80472479136013, -6.60979768468788 ] } },
+{ "type": "Feature", "properties": { "Name": "Asinan Sedap Gedung Dalam", "Desc": "Jalan Sukasari 1, RT.07\/RW.02, Sukasari, Bogor City, West Java", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.811868213124583, -6.617508829099396 ] } },
+{ "type": "Feature", "properties": { "Name": "Rumah Sakit PMI Bogor", "Desc": null, "X": 106.805091, "Y": -6.598688, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.805091, -6.598688 ] } },
+{ "type": "Feature", "properties": { "Name": "Vihara Dhanagun Bogor", "Desc": null, "X": 106.79922, "Y": -6.603344, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.79922, -6.603344 ] } },
+{ "type": "Feature", "properties": { "Name": "Gereja Kristus Bogor", "Desc": null, "X": 106.809337, "Y": -6.615028, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.809337, -6.615028 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Pertamina Jalan Veteran Bogor", "Desc": null, "X": 106.784008, "Y": -6.594842, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.784008, -6.594842 ] } },
+{ "type": "Feature", "properties": { "Name": "Polresta Kota Bogor", "Desc": null, "X": 106.791462, "Y": -6.596539, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.791462, -6.596539 ] } },
+{ "type": "Feature", "properties": { "Name": "Gereja Sidang Jemaat Allah Betlehem Bogor", "Desc": null, "X": 106.800797, "Y": -6.605817, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.800797, -6.605817 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Bogor Baru", "Desc": null, "X": 106.810538, "Y": -6.593647, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.810538, -6.593647 ] } },
+{ "type": "Feature", "properties": { "Name": "RS Bhayangkara Bogor", "Desc": null, "X": 106.791389, "Y": -6.596602, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.791389, -6.596602 ] } }
+]
+}
